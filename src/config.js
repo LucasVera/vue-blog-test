e8fefe959b41cd7e81102c479d6af7231004ef66
LucasVera <lucazzzv89@gmail.com>
2018-10-19 09:24:37 -0500

    backend url changed for https

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  ROOT_BACKEND_URL: 'http://www.lucasdev.info/api',
-  VUE_BLOG_BACKEND_URL: 'http://www.lucasdev.info/api/vueblog',
+  ROOT_BACKEND_URL: 'https://www.lucasdev.info/api',
+  VUE_BLOG_BACKEND_URL: 'https://www.lucasdev.info/api/vueblog',
 
   SECONDS_UNTIL_NEW_AUTOMATIC_FETCH: 2
 }
